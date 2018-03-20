5bb0b72091aa3acc9b2185d7d74afa8cd1477d9f
delta37 <delta37@delta37.delta37>
2018-03-21 02:27:00 +0300

    proxy_value

--- include/kaspar/Proxied/proxied.hpp
+++ include/kaspar/Proxied/proxied.hpp
@@ -3,42 +3,39 @@
 
 namespace kaspar {
 
-template <typename Container>
-class Proxied : public Container {
+template <typename value_type>
+class proxy_value {
 public:
-  typedef typename Container::value_type value_type;
-  struct proxy_value {
-    value_type* data;
-    proxy_value(void)=delete;
-    proxy_value(proxy_value const& other) {
-      data = other.data;
-    }
-    proxy_value(value_type& el) : data(&el) {}
-    operator value_type() {
-      std::cout << "read operation" << std::endl;
-      return *data;
-    }
-    proxy_value& operator=(proxy_value const& other) {
-      std::cout << "read and write operation" << std::endl;
-      *data = *other.data;
-      return *this;
-    }
-    proxy_value& operator=(value_type const& val) {
-      std::cout << "write operation" << std::endl;
-      *data = val;
-      return *this;
-    }
-  };
-
-  Proxied(Container const& c) : Container(c) {}
-  Proxied(Container&& c) : Container(c) {}
-  Proxied(void) : Container() {}
-  Proxied(Proxied<Container> const& other) : Container(other) {}
-  Proxied(Proxied<Container>&& other) : Container(other) {}
-
-  proxy_value operator[](size_t ind) {
-    return proxy_value(Container::operator[](ind));
+  proxy_value(void) {
+    data = new value_type();
   }
+  proxy_value(proxy_value<value_type> const& other) {
+    data = new value_type(*other.data);
+  }
+  proxy_value(value_type const& el) {
+    data = new value_type(el);
+  }
+  proxy_value(value_type& el) : data(&el) {}
+  operator value_type() {
+    std::cout << "read operation" << std::endl;
+    return *data;
+  }
+  proxy_value<value_type>& operator=(proxy_value<value_type> const& other) {
+    std::cout << "read and write operation" << std::endl;
+    *data = *other.data;
+    return *this;
+  }
+  proxy_value<value_type>& operator=(value_type const& val) {
+    std::cout << "write operation" << std::endl;
+    *data = val;
+    return *this;
+  }
+  virtual ~proxy_value(void) {
+    delete data;
+  }
+private:
+  value_type* data;
 };
+
 
 } // kaspar
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@ void f(double) {
 }
 
 int main(void) {
-  kaspar::Proxied<std::vector<int>> m({1,2});
-  m.push_back(1);
+  std::vector<kaspar::proxy_value<int>> m({1,2});
   int a = 3 * m[0];
   f(m[0]);
   m[0] = 2;
